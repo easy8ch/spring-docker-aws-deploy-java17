d4e8ee4c63b9cbdf1c81da0116d35521c3822f71
yiji-choi <yiji.choi@webtoonscorp.com>
2024-03-25 00:12:44 +0900

    Docker 빌드 관련 스크립트 추가

---
+++ scripts/deploy_docker.sh
@@ -0,0 +1,5 @@
+# TODO 각자 레포지토리 이름, Application 이름에 맞게 수정
+PROJECT_ROOT="/home/ec2-user/spring-docker-aws-deploy-java17"
+cp $PROJECT_ROOT/build/libs/tricountapi-0.0.1-SNAPSHOT.jar $PROJECT_ROOT/
+docker build -t tricount-api .
+docker run -d -p 8080:8080 tricount-api
